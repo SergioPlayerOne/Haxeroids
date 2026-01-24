41a2abb6328c8e208948492e026448a5d750803a
SergioPlayerOne <sergiolosan11@gmail.com>
2026-01-24 16:37:53 +0100

    Backup commit before big refactor

--- Source/Asteroid.hx
+++ Source/Asteroid.hx
@@ -1,3 +1,4 @@
+import haxe.macro.Expr.Case;
 import core.Vector2;
 import openfl.Vector;
 import openfl.display.Stage;
@@ -20,37 +21,46 @@ class Asteroid extends Sprite {
     private static inline var INITIAL_ASTEROIDS:Int = 3;
     private static inline var SPAWN_LOCATION_INCREMENT:Int = 90;
 
-    public var direction:Vector2 = new Vector2(0, 0);
+    public var direction:Vector2;
     public var radius:Int;
+    public var size:AsteroidSize;
 
     public function new(stage:Stage, size:AsteroidSize) {
         super();
 
-        var windowPerimeter:Int = stage.stageWidth * 2 + stage.stageHeight * 2;
-        var spawnPositionIndex = Random.int(0, windowPerimeter - 1);
+        this.size = size;
         this.radius = (() -> switch(size) {
             case Large: Random.int(MIN_RADIUS_LARGE, MAX_RADIUS_LARGE);
             case Medium: Random.int(MIN_RADIUS_MEDIUM, MAX_RADIUS_MEDIUM);
             case Small: Random.int(MIN_RADIUS_SMALL, MAX_RADIUS_SMALL);
         })();
 
-        // Checks to which side of the window the spawnPositionIndex is pointing at
-        if (spawnPositionIndex < stage.stageWidth) { // Top
-            this.y = -SPAWN_LOCATION_INCREMENT;
-            this.x = spawnPositionIndex;
-            this.direction = Vector2.fromAngle(Random.int(140, 220));
-        } else if (spawnPositionIndex < stage.stageWidth + stage.stageHeight) { // Right
-            this.x = stage.stageWidth + SPAWN_LOCATION_INCREMENT;
-            this.y = spawnPositionIndex - stage.stageWidth;
-            this.direction = Vector2.fromAngle(Random.int(-50, -130));
-        } else if (spawnPositionIndex < stage.stageWidth * 2 + stage.stageHeight) { // Bottom
-            this.y = stage.stageHeight + SPAWN_LOCATION_INCREMENT;
-            this.x = spawnPositionIndex - stage.stageHeight - stage.stageWidth;
-            this.direction = Vector2.fromAngle(Random.int(40, -40));
-        } else { // Left
-            this.x = -SPAWN_LOCATION_INCREMENT;
-            this.y = spawnPositionIndex - stage.stageWidth * 2 - stage.stageHeight;
-            this.direction = Vector2.fromAngle(Random.int(50, 130));
+        if (size == Large) { // New asteroids that spawns at the borders of the screen
+            var windowPerimeter:Int = stage.stageWidth * 2 + stage.stageHeight * 2;
+            var spawnPositionIndex = Random.int(0, windowPerimeter - 1);
+
+            // Checks to which side of the window the spawnPositionIndex is pointing at
+            if (spawnPositionIndex < stage.stageWidth) { // Top
+                this.y = -SPAWN_LOCATION_INCREMENT;
+                this.x = spawnPositionIndex;
+                this.direction = Vector2.fromAngle(Random.int(140, 220));
+            } else if (spawnPositionIndex < stage.stageWidth + stage.stageHeight) { // Right
+                this.x = stage.stageWidth + SPAWN_LOCATION_INCREMENT;
+                this.y = spawnPositionIndex - stage.stageWidth;
+                this.direction = Vector2.fromAngle(Random.int(-50, -130));
+            } else if (spawnPositionIndex < stage.stageWidth * 2 + stage.stageHeight) { // Bottom
+                this.y = stage.stageHeight + SPAWN_LOCATION_INCREMENT;
+                this.x = spawnPositionIndex - stage.stageHeight - stage.stageWidth;
+                this.direction = Vector2.fromAngle(Random.int(40, -40));
+            } else { // Left
+                this.x = -SPAWN_LOCATION_INCREMENT;
+                this.y = spawnPositionIndex - stage.stageWidth * 2 - stage.stageHeight;
+                this.direction = Vector2.fromAngle(Random.int(50, 130));
+            }
+        } else { // Asteroids created by another asteroid being destroyed
+            this.x = x;
+            this.y = y;
+
         }
 
         graphics.beginFill(0x5C5C5C);
@@ -74,6 +84,16 @@ class Asteroid extends Sprite {
     }
 
     public function onCollide() {
-        
+        switch (this.size) {
+            case Large: {
+
+            }
+            case Medium: {
+
+            }
+            case Small: {
+
+            }
+        }
     }
 }
